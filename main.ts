874387cbd352ee5d0fdd2543690cd3f8d5bbce8b
Patrick Lee <git@patricklee.nyc>
2023-02-25 11:40:56 -0500

    use internal app instance

--- main.ts
+++ main.ts
@@ -1,52 +1,54 @@
-import { Plugin } from "obsidian";
+import { Plugin, App } from "obsidian";
 
 export default class OpenInNewTabPlugin extends Plugin {
 	async onload() {
-		this.registerDomEvent(document, "click", this.handleClick, {
+		this.registerDomEvent(document, "click", this.generateClickHandler(this.app), {
 			capture: true,
 		});
 	}
 
-	handleClick(event: MouseEvent) {
-		const target = event.target as Element;
-		const isNavFile =
-			target?.classList?.contains("nav-file-title") ||
-			target?.classList?.contains("nav-file-title-content");
-		const titleEl = target?.closest(".nav-file-title");
+	generateClickHandler(appInstance: App) {
+		return function (event: MouseEvent) {
+			const target = event.target as Element;
+			const isNavFile =
+				target?.classList?.contains("nav-file-title") ||
+				target?.classList?.contains("nav-file-title-content");
+			const titleEl = target?.closest(".nav-file-title");
 
-		// Make sure it's just a left click so we don't interfere with anything.
-		const pureClick =
-			!event.shiftKey &&
-			!event.ctrlKey &&
-			!event.metaKey &&
-			!event.shiftKey &&
-			!event.altKey;
+			// Make sure it's just a left click so we don't interfere with anything.
+			const pureClick =
+				!event.shiftKey &&
+				!event.ctrlKey &&
+				!event.metaKey &&
+				!event.shiftKey &&
+				!event.altKey;
 
-		if (isNavFile && titleEl && pureClick) {
-			const path = titleEl.getAttribute("data-path");
-			if (path) {
-				// This logic is borrowed from the obsidian-no-dupe-leaves plugin
-				// https://github.com/patleeman/obsidian-no-dupe-leaves/blob/master/src/main.ts#L32-L46
-				let result = false;
-				this.app.workspace.iterateAllLeaves((leaf) => {
-					const viewState = leaf.getViewState();
-					if (viewState.state?.file === path) {
-						this.app.workspace.setActiveLeaf(leaf);
-						result = true;
+			if (isNavFile && titleEl && pureClick) {
+				const path = titleEl.getAttribute("data-path");
+				if (path) {
+					// This logic is borrowed from the obsidian-no-dupe-leaves plugin
+					// https://github.com/patleeman/obsidian-no-dupe-leaves/blob/master/src/main.ts#L32-L46
+					let result = false;
+					appInstance.workspace.iterateAllLeaves((leaf) => {
+						const viewState = leaf.getViewState();
+						if (viewState.state?.file === path) {
+							appInstance.workspace.setActiveLeaf(leaf);
+							result = true;
+						}
+					});
+
+					// If we have a "New Tab" tab open, just switch to that and let
+					// the default behavior open the file in that.
+					const emptyLeaves = appInstance.workspace.getLeavesOfType("empty");
+					if (emptyLeaves.length > 0) {
+						appInstance.workspace.setActiveLeaf(emptyLeaves[0]);
+						return;
 					}
-				});
 
-				// If we have a "New Tab" tab open, just switch to that and let
-				// the default behavior open the file in that.
-				const emptyLeaves = this.app.workspace.getLeavesOfType("empty");
-				if (emptyLeaves.length > 0) {
-					this.app.workspace.setActiveLeaf(emptyLeaves[0]);
-					return;
-				}
-
-				if (!result) {
-					event.stopPropagation(); // This might break something...
-					this.app.workspace.openLinkText(path, path, true);
+					if (!result) {
+						event.stopPropagation(); // This might break something...
+						appInstance.workspace.openLinkText(path, path, true);
+					}
 				}
 			}
 		}
